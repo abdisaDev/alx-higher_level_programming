35293161079fc53ad5d0e6b00944375cb9940985
Abdisa Dev <abdisaDev@gmail.com>
2024-03-15 23:55:23 +0300

    return last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    return number % 10
+    last_digit = abs(number) % 10
+    if number > 0:
+        return f"{last_digit:d}{last_digit:d}"
+    elif number <= 0:
+        return f"{last_digit:d}{last_digit:d}"
