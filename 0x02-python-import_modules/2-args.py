501053a7ac503b3b2b1bb38cd2f8166507b39a83
Abdisa Dev <abdisaDev@gmail.com>
2024-03-19 08:19:16 +0300

    main args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    else:
+        if len(sys.argv) == 2:
+            print("{:d} argument:".format(len(sys.argv) - 1))
+        else:
+            print("{:d} arguments:".format(len(sys.argv) - 1))
+        for index in range(1, len(sys.argv)):
+            print("{:d}: {:s}".format(index, sys.argv[index]))
