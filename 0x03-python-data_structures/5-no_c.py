b78f83f47270ceddd7e629cebeeaa2feac6c2dbd
Abdisa Dev <abdisaDev@gmail.com>
2024-03-20 08:00:01 +0300

    ga

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = None
+    new_string = ""
     for char in my_string:
         if char == "c" or char == "C":
             continue
         else:
-            new_string = char
+            new_string += char
     return new_string
