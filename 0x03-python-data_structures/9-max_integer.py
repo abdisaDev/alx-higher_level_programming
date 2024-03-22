b7ec65d5ee1946f2ae97d0968d18f8dd40691443
Abdisa Dev <abdisaDev@gmail.com>
2024-03-22 14:03:23 +0300

    bigggggg

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     big = my_list[0]
-    if len(my_list) <= 0:
+    if len(my_list) == 0:
         return None
     for num in my_list:
         if num > big:
