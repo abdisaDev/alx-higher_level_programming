040ee66fed30ec09f75f7f904ba3a30b8df01d9b
Abdisa Dev <abdisaDev@gmail.com>
2024-03-24 13:02:24 +0300

    diff in elements

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    a = list(filter(lambda x: x if x not in set_2 else "",set_1))
-    b = list(filter(lambda y: y if y not in set_1 else "",set_2))
+    a = list(filter(lambda x: x if x not in set_2 else "", set_1))
+    b = list(filter(lambda y: y if y not in set_1 else "", set_2))
     a.extend(b)
     return a
