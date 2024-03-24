2d2e2293f515a0680c92791306b5284e772f7315
Abdisa Dev <abdisaDev@gmail.com>
2024-03-24 16:35:28 +0300

    max scored

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     max = (0, "")
-    if a_dictionary == None or len(a_dictionary) == 0:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     for x, y in a_dictionary.items():
         if y > max[0]:
