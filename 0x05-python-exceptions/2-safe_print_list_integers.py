55e96970fdfe311dc371da942a8e3fbe16d12021
Abdisa Dev <abdisaDev@gmail.com>
2024-04-14 13:07:25 +0300

    safe print list integer

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    total = 0
+    for element in range(x):
+        try:
+            print("{:d}".format(my_list[element]), end="")
+            total += 1
+        except (IndexError, ValueError, TypeError):
+            continue
+    return total
