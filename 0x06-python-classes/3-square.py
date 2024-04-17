2a65dc3bbe63eae9504b0956037b65f92e720df0
AbdisaDev <AbdisaDev@gmail.com>
2024-04-17 08:13:34 +0300

    return square of square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Define a class Square """
+
+
+class Square:
+    """ Represent class Square """
+    def __init__(self, size=0):
+        """
+            Initalize constructor for class Square
+            with default size attribute (0)
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Area method to return the are of the given square """
+        return self.__size ** 2
