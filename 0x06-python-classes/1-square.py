0a99faea1e8f34526693982c3338a3f69e6129df
AbdisaDev <AbdisaDev@gmail.com>
2024-04-17 08:00:06 +0300

    square with size instance attr

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Define a class Square """
+
+
+class Square:
+    """ Represent class Square """
+    def __init__(self, size):
+        """ Initalize constructor for class Square """
+        self.__size = size
