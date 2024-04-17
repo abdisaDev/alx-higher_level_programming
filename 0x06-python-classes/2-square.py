edbd5af8af8f9977e58c5efcc21031bbf0d3bdf0
AbdisaDev <AbdisaDev@gmail.com>
2024-04-17 08:09:42 +0300

    square with validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,9 +9,8 @@ class Square:
             Initalize constructor for class Square
             with default size attribute (0)
         """
-        try:
-            if (size < 0):
-                raise ValueError("size must be >= 0")
-            self.__size = size
-        except TypeError:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
