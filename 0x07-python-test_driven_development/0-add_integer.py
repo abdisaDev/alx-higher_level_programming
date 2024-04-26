f4fab31bc1d8e52ffdb35ca0d86d9381a757b4bf
AbdisaDev <AbdisaDev@gmail.com>
2024-04-26 09:10:44 +0300

    satisfaied linter

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,7 +7,7 @@ def add_integer(a, b=98):
     """ Two Number Adding Function """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    elif not isinstance(b, (int, float)):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return (int(a) + int(b))
 
