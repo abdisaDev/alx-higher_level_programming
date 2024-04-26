ff7a85a317fcf0139383b9b4e8b764be598f1f20
AbdisaDev <AbdisaDev@gmail.com>
2024-04-26 09:33:38 +0300

    elif

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,7 +6,7 @@ def add_integer(a, b=98):
     """ Two Number Adding Function """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return (int(a) + int(b))
 
