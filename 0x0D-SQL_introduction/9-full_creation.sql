e4ad0a63e139b1d20be2faf71900bd91d6d7489e
Abdisa Dev <abdisaDev@gmail.com>
2024-06-08 08:06:07 +0300

    db creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- full db creation
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table(id, name, score) 
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `second_table`(`id`, name, `score`) 
 VALUES 
     (1, 'Jhon', 10), 
     (1, 'Alex', 3), 
