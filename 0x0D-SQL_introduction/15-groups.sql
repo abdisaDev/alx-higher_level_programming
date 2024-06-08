44909678098953c682e557610550c582b260c871
Abdisa Dev <abdisaDev@gmail.com>
2024-06-08 07:42:12 +0300

    group by

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- duplication counter
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score HAVING number >= 1;
+SELECT `score`, COUNT(*) AS number FROM `second_table` GROUP BY `score` ORDER BY `score` DESC;
