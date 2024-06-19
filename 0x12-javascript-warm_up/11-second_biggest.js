15b26b76d44b4e8fbfbc18af24a42920c9899e43
Abdisa Dev <AbdisaDev@gmail.com>
2024-06-19 11:38:49 +0300

    semistandar

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,7 +6,7 @@ const lists = argv.splice(2);
 if (lists.length <= 1) {
   console.log(0);
 } else {
-  const max_digit = Math.max(...lists);
-  lists[lists.indexOf(String(max_digit))] = Number.NEGATIVE_INFINITY;
+  const maxDigit = Math.max(...lists);
+  lists[lists.indexOf(String(maxDigit))] = Number.NEGATIVE_INFINITY;
   console.log(Math.max(...lists));
 }
