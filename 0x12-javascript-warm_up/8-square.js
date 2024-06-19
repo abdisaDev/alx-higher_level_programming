c5dda9f74adc6683b5d3d7421fcbb1005317c35a
Abdisa Dev <AbdisaDev@gmail.com>
2024-06-19 10:21:32 +0300

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,10 +3,11 @@ const { argv, stdout } = require('node:process');
 const size = Number(argv[2]);
 if (isNaN(size)) {
   console.log('Missing size');
-} else
+} else {
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
       stdout.write('X');
     }
     console.log();
   }
+}
