62025c642b10a577694c1fb1b3a20da5e3ba65b4
Abdisa Dev <AbdisaDev@gmail.com>
2024-06-20 15:18:26 +0300

    file system

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,7 +2,7 @@
 const { readFileSync, writeFileSync } = require('fs');
 const { argv } = require('node:process');
 
-const first = readFileSync(argv[2]);
-const second = readFileSync(argv[3]);
+const first = readFileSync(argv[2], 'utf-8');
+const second = readFileSync(argv[3], 'utf-8');
 
 writeFileSync(argv[4], first + second);
