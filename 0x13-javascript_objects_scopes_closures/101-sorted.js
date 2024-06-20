87f67f6b7588681e4d075e95f049b0653bd09cc4
Abdisa Dev <AbdisaDev@gmail.com>
2024-06-20 15:07:15 +0300

    dsadsa

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -4,9 +4,10 @@ const { dict } = require('./101-data.js');
 const newDict = {};
 
 for (const key in dict) {
-  if (typeof newDict[dict[key]] === "undefined") {
+  if (newDict[dict[key]] === undefined) {
     newDict[dict[key]] = [];
   }
-
   newDict[dict[key]].push(key);
 }
+
+console.log(newDict);
